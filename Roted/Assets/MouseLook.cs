5627e960d8ba24a24c7d138e02a63294357b7739
TeeZ0 <31013801+TeeZ0@users.noreply.github.com>
2017-08-16 02:34:02 +0200

    Movement, Mouse look

---
+++ Roted/Assets/MouseLook.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+
+    public float Sensitivity = 6;
+    public Camera cam;
+    public bool lockCursor = false;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    float rotationX = 0F;
+    float rotationY = 0F;
+    Quaternion originalRotation;
+    // Use this for initialization
+    void Start () {
+        lockCursor = true;
+        originalRotation = transform.localRotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (lockCursor)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
+
+        // Read the mouse input axis
+        rotationX += Input.GetAxis("Mouse X") * Sensitivity;
+        rotationY += Input.GetAxis("Mouse Y") * Sensitivity;
+        rotationX = ClampAngle(rotationX, minimumX, maximumX);
+        rotationY = ClampAngle(rotationY, minimumY, maximumY);
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+
+
+
+
+        transform.localRotation = Quaternion.Euler(new Vector3(0, cam.transform.rotation.eulerAngles.y, 0));
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y +.6f, transform.position.z);
+        cam.transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+
+
+
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+         angle += 360F;
+        if (angle > 360F)
+         angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Roted/Assets/Movement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController character;
+    private Rigidbody rb;
+    public float forwardSpeed = 5;
+    public float backwardSpeed = 4;
+    public float sidewaySpeed = 4;
+    public float sprintSpeedMultiplier = 1.5f;
+    public float jumpForce = 30;
+    public bool isSprinting = false;
+    private float curSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (character.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Vector3 up = transform.TransformDirection(Vector3.up);
+                float curJump = jumpForce * 1;
+                character.SimpleMove(up * curJump);
+            }
+        }
+
+
+        isSprinting = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            //Going forward
+            if (isSprinting)
+            {
+                curSpeed = forwardSpeed * Input.GetAxis("Vertical");
+                curSpeed *= sprintSpeedMultiplier;
+            }
+            else
+                curSpeed = forwardSpeed * Input.GetAxis("Vertical");
+        }
+        else
+        {
+            //Going backwards
+            curSpeed = backwardSpeed * Input.GetAxis("Vertical");
+        }
+        character.SimpleMove(forward * curSpeed);
+
+        float curSpeed2 = sidewaySpeed * Input.GetAxis("Horizontal");
+        character.SimpleMove(right * curSpeed2);
+    }
+
+   
+}
+    
